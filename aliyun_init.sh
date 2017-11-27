2def450a0c003f5060c81553fbe125890f428d1a
Judy Liu <judy.liu@b2be.com>
2017-11-27 19:01:57 +0800

    mod.

--- aliyun_init.sh
+++
@@ -1,3 +0,0 @@
-cd /root/nsfw && git pull origin master
-mount -t nfs 172.17.216.110:/mnt /mnt -o proto=tcp -o nolock
-docker run -it -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt --name nsfw bvlc/caffe:cpu sh run.sh
