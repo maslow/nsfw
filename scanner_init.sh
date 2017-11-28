0f5e9d95791ed9d67eb816c655df2deb7b9cafa6
Judy Liu <judy.liu@b2be.com>
2017-11-28 12:19:02 +0800

    MOD SHELL

--- scanner_init.sh
+++ scanner_init.sh
@@ -4,7 +4,7 @@ export REDIS_HOST=172.17.216.169
 cd /root/nsfw && git pull origin master
 mount -t nfs $REDIS_HOST:/mnt /mnt -o proto=tcp -o nolock
 
-docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt --name nsfw bvlc/caffe:cpu sh run.sh &
-docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt --name nsfw1 bvlc/caffe:cpu sh run.sh &
-docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt --name nsfw2 bvlc/caffe:cpu sh run.sh &
-docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt --name nsfw3 bvlc/caffe:cpu sh run.sh &
+docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt -e REDIS_HOST=$REDIS_HOST --name nsfw bvlc/caffe:cpu sh run.sh
+docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt -e REDIS_HOST=$REDIS_HOST --name nsfw1 bvlc/caffe:cpu sh run.sh
+docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt -e REDIS_HOST=$REDIS_HOST --name nsfw2 bvlc/caffe:cpu sh run.sh
+docker run -d -v /root/nsfw/open_nsfw:/workspace -v /mnt:/mnt -e REDIS_HOST=$REDIS_HOST --name nsfw3 bvlc/caffe:cpu sh run.sh
--- spider_init.sh
+++ spider_init.sh
@@ -4,7 +4,7 @@ export REDIS_HOST=172.17.216.169
 systemctl start rpcbind
 systemctl start nfs
 
-cd /root && git clone https://github.com/Maslow/nsfw.git
+cd /root/nsfw && git pull origin master
 cd /root/nsfw/spider && npm install
 
-docker run -d -p 6379:6379 -v /mnt/redis-data:/data --name redis-server redis &
+docker run -d -p 6379:6379 -v /mnt/redis-data:/data redis
